91671f6b1aef88a047dc00119fe41bd6757775d1
Nadia <nlnadialigia@gmail.com>
2021-03-15 23:47:13 -0300

    feat: add categories repository

---
+++ src/repositories/CategoriesRepository.ts
@@ -0,0 +1,28 @@
+import { Category } from '../model/Category';
+
+interface ICreateCategoryDTO {
+  name: string;
+  description: string;
+}
+
+class CategoryRepository {
+  private categories: Category[];
+
+  constructor() {
+    this.categories = [];
+  }
+
+  create({ name, description }: ICreateCategoryDTO): void {
+    const category = new Category();
+
+    Object.assign(category, {
+      name,
+      description,
+      create_at: new Date(),
+    });
+
+    this.categories.push(category);
+  }
+}
+
+export { CategoryRepository };
--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,22 +1,13 @@
 import { Router } from 'express';
-import { Category } from '../model/Category';
+import { CategoryRepository } from '../repositories/CategoriesRepository';
 
 const categoriesRoutes = Router();
-
-const categories: Category[] = [];
+const categoriesRepository = new CategoryRepository();
 
 categoriesRoutes.post('/', (request, response) => {
   const { name, description } = request.body;
 
-  const category = new Category();
-
-  Object.assign(category, {
-    name,
-    description,
-    create_at: new Date(),
-  });
-
-  categories.push(category);
+  categoriesRepository.create({ name, description });
 
   return response.status(201).send();
 });
