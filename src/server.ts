dd56b2ede4a6ca3cc8aea8148d73ba8ed6eae577
Nadia <nlnadialigia@gmail.com>
2021-03-15 21:14:42 -0300

    feat: add debug config

--- src/server.ts
+++ src/server.ts
@@ -2,8 +2,16 @@ import express from "express";
 
 const app = express();
 
+app.use(express.json());
+
 app.get("/", (request, response) => {
   return response.json({ message: "Hello World" });
+});
+
+app.post("/courses", (request, response) => {
+  const { name } = request.body;
+
+  return response.json({ name });
 });
 
 app.listen(3380, () => console.log("Server is running"));
