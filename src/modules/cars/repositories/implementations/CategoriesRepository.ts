19a7fda6aeaa576ed02d8fbd9ab0191a7493dc6e
Nadia <nlnadialigia@gmail.com>
2021-03-17 07:22:22 -0300

    refactor: add implentantions folder to repositories folder

--- src/modules/cars/repositories/implementations/CategoriesRepository.ts
+++ src/modules/cars/repositories/implementations/CategoriesRepository.ts
@@ -1,9 +1,8 @@
-import { Category } from '../model/Category';
+import { Category } from '../../model/Category';
 import {
   ICategoriesRepository,
-  // eslint-disable-next-line prettier/prettier
   ICreateCategoryDTO
-} from './ICategoriesRepository';
+} from '../ICategoriesRepository';
 
 class CategoriesRepository implements ICategoriesRepository {
   private categories: Category[];
