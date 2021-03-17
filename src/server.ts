8ea181c0bde87fd5590ffef00fd6c69b96422044
Nadia <nlnadialigia@gmail.com>
2021-03-17 09:13:17 -0300

    feat: add index to routes folder

--- src/server.ts
+++ src/server.ts
@@ -1,21 +1,9 @@
 import express from 'express';
-import { categoriesRoutes } from './routes/categories.routes';
-import { specificationsRoutes } from './routes/specifications.routes';
+import { router } from './routes';
 
 const app = express();
 
 app.use(express.json());
-app.use('/categories', categoriesRoutes);
-app.use('/specifications', specificationsRoutes);
-
-app.get('/', (request, response) => {
-  return response.json({ message: 'Hello World' });
-});
-
-app.post('/courses', (request, response) => {
-  const { name } = request.body;
-
-  return response.json({ name });
-});
+app.use(router);
 
 app.listen(3380, () => console.log('Server is running'));
