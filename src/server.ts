b6a08cff512eec4fe6cd08c21a5287eae8726366
Nadia <nlnadialigia@gmail.com>
2021-03-17 13:49:16 -0300

    chore: add swagger and config

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,12 @@
 import express from 'express';
+import swaggerUi from 'swagger-ui-express';
 import { router } from './routes';
+import swaggerFile from './swagger.json';
 
 const app = express();
 
 app.use(express.json());
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile));
 app.use(router);
 
 app.listen(3380, () => console.log('Server is running'));
