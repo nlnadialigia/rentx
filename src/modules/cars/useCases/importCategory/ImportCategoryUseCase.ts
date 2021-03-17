6736c784d18ffc3e5499cf4e585bfd551335e3a3
Nadia <nlnadialigia@gmail.com>
2021-03-17 15:15:16 -0300

    refactor: remove file after push

--- src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
+++ src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
@@ -29,6 +29,7 @@ class ImportCategoryUseCase {
           });
         })
         .on('end', () => {
+          fs.promises.unlink(file.path);
           resolve(categories);
         })
         .on('error', (error) => {
