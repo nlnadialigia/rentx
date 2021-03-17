56920d4348e15be642abf3f6ff7b85da4c2ba92b
Nadia <nlnadialigia@gmail.com>
2021-03-17 10:55:59 -0300

    feat: add sream and scvParse to read the file

--- src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
+++ src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
@@ -1,6 +1,17 @@
+import csvParse from 'csv-parse';
+import fs from 'fs';
+
 class ImportCategoryUseCase {
-  execute(file: any) {
-    console.log(file);
+  execute(file: Express.Multer.File): void {
+    const stream = fs.createReadStream(file.path);
+
+    const parseFile = csvParse();
+
+    stream.pipe(parseFile);
+
+    parseFile.on('data', async (line) => {
+      console.log(line);
+    });
   }
 }
 
