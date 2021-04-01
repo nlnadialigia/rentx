7ab666dfa512b8dfc64a4050a71a50542cd36cc3
Nadia <nlnadialigia@gmail.com>
2021-04-01 20:04:25 -0300

    test: ensure CreateCarSpecificationUseCase don't create an existent specification for the same car

---
+++ src/modules/cars/repositories/in-memory/SpecificationsRepositoryInMemory.ts
@@ -0,0 +1,40 @@
+import { Specification } from '@modules/cars/infra/typeorm/entities/Specification';
+import {
+  ICreateSpecificationDTO,
+  ISpecificationsRepository
+} from '../ISpecificationsRepository';
+
+class SpecificationsRepositoryInMemory implements ISpecificationsRepository {
+  specifications: Specification[] = [];
+  async create({
+    name,
+    description
+  }: ICreateSpecificationDTO): Promise<Specification> {
+    const specification = new Specification();
+
+    Object.assign(specification, {
+      name,
+      description
+    });
+
+    this.specifications.push(specification);
+
+    return specification;
+  }
+
+  async findByName(name: string): Promise<Specification> {
+    return this.specifications.find(
+      (specification) => specification.name === name
+    );
+  }
+
+  async findByIds(ids: string[]): Promise<Specification[]> {
+    const specifications = this.specifications.filter((specification) =>
+      ids.includes(specification.id)
+    );
+
+    return specifications;
+  }
+}
+
+export { SpecificationsRepositoryInMemory };
--- src/modules/cars/useCases/createCarSpecification/CreateCarSpecificationUseCase.spec.ts
+++ src/modules/cars/useCases/createCarSpecification/CreateCarSpecificationUseCase.spec.ts
@@ -1,10 +1,12 @@
 import crypto from 'crypto';
 import { CarsRepositoryInMemory } from '@modules/cars/repositories/in-memory/CarsRepositoryInMemory';
+import { SpecificationsRepositoryInMemory } from '@modules/cars/repositories/in-memory/SpecificationsRepositoryInMemory';
 import { AppError } from '@shared/errors/AppError';
 import { CreateCarSpecificationUseCase } from './CreateCarSpecificationUseCase';
 
 let createCarSpecificationUseCase: CreateCarSpecificationUseCase;
 let carsRepositoryInMemory: CarsRepositoryInMemory;
+let specificationsRepositoryInMemory: SpecificationsRepositoryInMemory;
 
 const generate = () => {
   return crypto.randomBytes(20).toString('hex');
@@ -13,8 +15,10 @@ const generate = () => {
 describe('Specifications Car', () => {
   beforeEach(() => {
     carsRepositoryInMemory = new CarsRepositoryInMemory();
+    specificationsRepositoryInMemory = new SpecificationsRepositoryInMemory();
     createCarSpecificationUseCase = new CreateCarSpecificationUseCase(
-      carsRepositoryInMemory
+      carsRepositoryInMemory,
+      specificationsRepositoryInMemory
     );
   });
 
@@ -29,7 +33,12 @@ describe('Specifications Car', () => {
       category_id: generate()
     });
 
-    const specification_id = [generate()];
+    const specification = await specificationsRepositoryInMemory.create({
+      name: generate(),
+      description: generate()
+    });
+
+    const specification_id = [specification.id];
 
     createCarSpecificationUseCase.execute({
       car_id: car.id,
