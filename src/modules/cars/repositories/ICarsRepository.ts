5357b13e9af333525740a7a1cd0c88014cb6e26b
Nadia <nlnadialigia@gmail.com>
2021-04-15 09:55:04 -0300

    feat: ensure ListCarsuseCase list all available cars

--- src/modules/cars/repositories/ICarsRepository.ts
+++ src/modules/cars/repositories/ICarsRepository.ts
@@ -4,6 +4,7 @@ import { Car } from '../infra/typeorm/entities/Car';
 interface ICarsRepository {
   create(data: ICreateCarDTO): Promise<Car>;
   findByLincensePlate(license_plate: string): Promise<Car>;
+  findAvailable(): Promise<Car[]>;
 }
 
 export { ICarsRepository };
---
+++ src/modules/cars/useCases/listCars/ListCarsUseCase.ts
@@ -0,0 +1,13 @@
+import { Car } from '@modules/cars/infra/typeorm/entities/Car';
+import { ICarsRepository } from '@modules/cars/repositories/ICarsRepository';
+
+class ListCarsUseCase {
+  constructor(private carsRepository: ICarsRepository) { }
+  async execute(): Promise<Car[]> {
+    const cars = await this.carsRepository.findAvailable();
+
+    return cars;
+  }
+}
+
+export { ListCarsUseCase };
